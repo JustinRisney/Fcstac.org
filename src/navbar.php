3f85d21f93380dc67f1e60c37a11447f7768c684
JustinRisney <31742964+JustinRisney@users.noreply.github.com>
2017-10-05 12:08:08 -0700

    Copied Navbar

--- src/navbar.php
+++ src/navbar.php
@@ -20,7 +20,40 @@
 		FCSTAC	
 	</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
+		<span class="navbar-toggler-icon">
+			
+		</span>
 	</button>
+
+	<div class="collapse navbar-collapse" id="nav-content">   
+		<ul class="navbar-nav">
+			
+			<li class="nav-item ">
+				<a class="nav-link" href="/about">About</a>
+			</li>
+			<li class="nav-item ">
+				<a class="nav-link" href="/news">News</a>
+			</li>
+			<li class="nav-item ">
+				<a class="nav-link" href="/recognition-awards">Recognition & Awards</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/advocacy">Advocacy</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/professional-development">Professional Development</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/resources">Resources</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/join-renew">Join/Renew</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/contact">Contact</a>
+			</li>
+		</ul>
+
+	</div>
 	
 </nav>
