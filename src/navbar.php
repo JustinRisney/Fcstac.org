afbe651ee402c84477a6504d016194e06823e521
adumbidiot <nathaniel.daniel23@outlook.com>
2017-11-03 11:39:04 -0700

    navbar recolor

--- src/navbar.php
+++ src/navbar.php
@@ -15,7 +15,7 @@
 		font-weight: 300;	
 	} 
 	</style>
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top" id="OUR-navbar">
+<nav class="navbar navbar-expand-sm navbar-dark fixed-top" style="background-color: #2060C7" id="OUR-navbar">
 	<a class="navbar-brand" href="/">
 		FCSTAC	
 	</a>
@@ -28,28 +28,28 @@
 	<div class="collapse navbar-collapse" id="nav-content">   
 		<ul class="navbar-nav">
 			
-			<li class="nav-item ">
+			<li class="nav-item <?php setnavactive($url, "/about"); ?>">
 				<a class="nav-link" href="/about">About</a>
 			</li>
-			<li class="nav-item ">
+			<li class="nav-item <?php setnavactive($url, "/news"); ?>">
 				<a class="nav-link" href="/news">News</a>
 			</li>
-			<li class="nav-item ">
+			<li class="nav-item <?php setnavactive($url, "/recognition-awards"); ?>">
 				<a class="nav-link" href="/recognition-awards">Recognition & Awards</a>
 			</li>
-			<li class="nav-item">
+			<li class="nav-item <?php setnavactive($url, "/advocacy"); ?>">
 				<a class="nav-link" href="/advocacy">Advocacy</a>
 			</li>
-			<li class="nav-item">
+			<li class="nav-item <?php setnavactive($url, "/professional-development"); ?>">
 				<a class="nav-link" href="/professional-development">Professional Development</a>
 			</li>
-			<li class="nav-item">
+			<li class="nav-item <?php setnavactive($url, "/resources"); ?>">
 				<a class="nav-link" href="/resources">Resources</a>
 			</li>
-			<li class="nav-item">
+			<li class="nav-item <?php setnavactive($url, "/join-renew"); ?>">
 				<a class="nav-link" href="/join-renew">Join/Renew</a>
 			</li>
-			<li class="nav-item">
+			<li class="nav-item <?php setnavactive($url, "/contact"); ?>">
 				<a class="nav-link" href="/contact">Contact</a>
 			</li>
 		</ul>
