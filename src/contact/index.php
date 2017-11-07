80aa70dcae5f7d2c72c81e0dac7bb9cc9457f837
Nathaniel Daniel <9022440@ROCKLIN.K12.CA.US>
2017-11-07 12:02:16 -0800

    Contacts page work

--- src/contact/index.php
+++ src/contact/index.php
@@ -9,23 +9,49 @@
     </head>
     <body>
 		<?php include'../navbar.php';?>
-		<div class="container text-center" style="padding: 80px 40px;">
+		<div class="container-fluid text-center" style="padding: 80px 0px;">
 			<h1 style="font-weight: 200;">Executive Board</h1>
-			<p style="padding: 20px 20px;">Dawn Maceyka------President-------hetac01@gmail.com</p>
-		</div>
-		<p>
-Dawn Maceyka- President:                              
-Tanya Durham Wright-Vice-President:        tanya@jtwright.net
-Linda Johnson-Secretary:                                lljohn@pacbell.net
-Linda Rosensteel-Treasurer:                           linda.rosensteel@hesperiausd.org
-Raean Gaddoni- Web Master:                         raean.gaddoni@acusd.org
-Mona Klein- Legislative Liaison:                    mona.klein@comcast.net
-REGIONAL COMMUNICATION NETWORK
-Click on your region to contact your Communication Chair, 
+
+			<p style="padding: 20px 20px;">Linda Rosensteel-----Treasurer-----linda.rosensteel@hesperiausd.org</p>
+			<p style="padding: 20px 20px;">Raean Gaddoni------Web Master-------raean.gaddoni@acusd.org</p>
+			<p style="padding: 20px 20px;">Mona Klein------Legislative Liaison-----mona.klein@comcast.net</p>
+			<h1 style="font-weight: 200;">Regional Communication Network</h1>
+			<p>                             
+Click on your region to contact your Communication Chair,
 and to see what events are coming up.</p>
-
-
-
+		</div>
+		
+<table class="table table-responsive-md">
+				<thead>
+					<tr>
+						<th scope="col">#</th>
+						<th scope="col">Name</th>
+						<th scope="col">Position</th>
+						<th scope="col">Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row">1</th>
+						<td>Dawn Maceyka</td>
+						<td>President</td>
+						<td>hetac01@gmail.com</td>
+					</tr>
+					<tr>
+						<th scope="row">2</th>
+						<td>Tanya Durham Wright</td>
+						<td>Vice-President</td>
+						<td>tanya@jtwright.net</td>
+					</tr>
+					<tr>
+						<th scope="row">3</th>
+						<td>Linda Johnson</td>
+						<td>Secretary</td>
+						<td>lljohn@pacbell.net</td>
+					</tr>
+				</tbody>
+			</table>
+			
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
