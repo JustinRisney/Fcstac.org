503d2d413ffe4af9663350526aad74319ca749fa
JustinRisney <31742964+JustinRisney@users.noreply.github.com>
2017-11-07 11:21:56 -0800

    News list

--- src/news/index.php
+++ src/news/index.php
@@ -9,6 +9,91 @@
     </head>
     <body>
       <?php include'../navbar.php';?>
+            <h1>Archived News Letters</h1>
+
+            <div id="link-container">
+            <ul>
+                <li class="Year-published">
+                    <h4>2016</h4>
+                </li>
+                <li class="Links">
+                    <a href="">
+                        December Newsletter
+                    </a>
+                </li>
+                <li class="Links">
+                    <a href="">
+                        September Newsletter
+                    </a>
+                </li>
+                <li class="Links">
+                    <a href="">
+                        May Newsletter
+                    </a>
+                </li>
+
+                <li class="Year-published">
+                    <h4> 2015 </h4>
+                </li>
+
+                <li class="Links">
+                    <a href="">
+                        Summer Newsletter
+                </li>
+                 <li class="Links">
+                    <a href="">
+                        Fall Newsletter
+                </li>
+                 <li class="Links">
+                    <a href="">
+                        Winter Newsletter
+                </li>
+                 <li class="Links">
+                    <a href="">
+                        Spring Newsletter
+                </li>
+
+                 <li class="Year-published">
+                    <a href="">
+                      <h4> 2014 </h4>
+                </li>
+                 <li class="Links">
+                    <a href="">
+                        Winter Newsletter
+                </li>
+                 <li class="Links">
+                    <a href="">
+                        Spring Newsletter
+                </li>
+                 <li class="Links">
+                    <a href="">
+                        Summer Newsletter
+                </li>
+
+                 <li class="Year-published">
+                    <a href="">
+                        <h4> 2013 </h4>
+                </li>
+                 <li class="Links">
+                    <a href="">
+                        Spring Newsletter
+                </li>
+                 <li class="Links">
+                    <a href="">
+                        Fall Newsletter
+                </li>
+
+                 <li class="Year-published">
+                    <a href="">
+                        <h4>2012</h4>
+                </li>
+                 <li class="Links">
+                    <a href="">
+                        Fall Newsletter
+                </li>
+            </ul>
+            </div>
+
 
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
