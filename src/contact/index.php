910bb204967062be27a9916c09d272fa6c86f712
Nathaniel Daniel <9022440@ROCKLIN.K12.CA.US>
2017-11-09 12:05:43 -0800

    Table wokr

--- src/contact/index.php
+++ src/contact/index.php
@@ -6,51 +6,72 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" type="text/css" href="stylesheets/index.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-    </head>
+		<style>
+			.table-dark td, .table-dark th, .table-dark thead th{
+				color: #ffffff;
+				background-color: #212529;
+				border-color: #32383e;
+			}
+		</style>
+	</head>
     <body>
 		<?php include'../navbar.php';?>
-		<div class="container-fluid text-center" style="padding: 80px 0px;">
-			<h1 style="font-weight: 200;">Executive Board</h1>
-
-			<p style="padding: 20px 20px;">Linda Rosensteel-----Treasurer-----linda.rosensteel@hesperiausd.org</p>
-			<p style="padding: 20px 20px;">Raean Gaddoni------Web Master-------raean.gaddoni@acusd.org</p>
-			<p style="padding: 20px 20px;">Mona Klein------Legislative Liaison-----mona.klein@comcast.net</p>
-			<h1 style="font-weight: 200;">Regional Communication Network</h1>
-			<p>                             
-Click on your region to contact your Communication Chair,
-and to see what events are coming up.</p>
-		</div>
-		
-<table class="table table-responsive-md">
+		<div style="padding: 80px 0px;">
+			<div class="text-center">
+				<h1 style="font-weight: 200;">Executive Board</h1>
+			</div>
+			<div style="padding: 80px 0px;">
+				<table class="table table-responsive-md table-dark table-hover">
 				<thead>
 					<tr>
-						<th scope="col">#</th>
 						<th scope="col">Name</th>
 						<th scope="col">Position</th>
 						<th scope="col">Email</th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<th scope="row">1</th>
-						<td>Dawn Maceyka</td>
-						<td>President</td>
-						<td>hetac01@gmail.com</td>
-					</tr>
-					<tr>
-						<th scope="row">2</th>
-						<td>Tanya Durham Wright</td>
-						<td>Vice-President</td>
-						<td>tanya@jtwright.net</td>
-					</tr>
-					<tr>
-						<th scope="row">3</th>
-						<td>Linda Johnson</td>
-						<td>Secretary</td>
-						<td>lljohn@pacbell.net</td>
-					</tr>
-				</tbody>
+				<tr>
+					<td>Dawn Maceyka</td>
+					<td>President</td>
+					<td>hetac01@gmail.com</td>
+				</tr>
+				<tr>
+					<td>Tanya Durham Wright</td>
+					<td>Vice-President</td>
+					<td>tanya@jtwright.net</td>
+				</tr>
+				<tr>
+					<td>Linda Johnson</td>
+					<td>Secretary</td>
+					<td>lljohn@pacbell.net</td>
+				</tr>
+				<tr>
+					<td>Linda Rosensteel</td>
+					<td>Treasurer</td>
+					<td>linda.rosensteel@hesperiausd.org</td>
+				</tr>
+				<tr>
+					<td>Jacob Boyce</td>
+					<td>Web Master</td>
+					<td>jacob.boyce@rocklinusd.org</td>
+				</tr>
+				<tr>
+					<td>Mona Klein</td>
+					<td>Legislative Liaison</td>
+					<td>mona.klein@comcast.net</td>
+				</tr>
+			</tbody>
 			</table>
+			</div>
+		</div>
+		<div class="container-fluid text-center">
+			<h1 ></h1>
+			<h1 style="font-weight: 200;">Regional Communication Network</h1>
+			<p>                             
+Click on your region to contact your Communication Chair,
+and to see what events are coming up.</p>
+		</div>
+		
 			
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
