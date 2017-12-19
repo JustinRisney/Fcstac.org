902b7a57c40c5db1d917e8d3e5d24a3476585403
JustinRisney <31742964+JustinRisney@users.noreply.github.com>
2017-12-19 12:00:04 -0800

    Did some links - still need to fix retardation

--- src/resources/index.php
+++ src/resources/index.php
@@ -4,12 +4,94 @@
     		<title>FCTAC</title>
     		<meta charset="utf-8">
     		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    		<link rel="stylesheet" type="text/css" href="stylesheets/index.css">
     		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="/stylesheets/resources.css">
   	</head>
   	<body>
    		<?php include'../navbar.php';?>
+        <div class="container text-center">
+          <h1>FCSTAC</h1>
+          <h3>Resources</h3>
 
+
+          <ul class="text-center" style="padding: 0px; list-style: none;">
+            <a target="_blank" href="http://hect.org">
+              <li class="links">HECT Home Page</li>
+            </a>
+
+            <a target="_blank" href="http://www.ca-fhahero-fccla.org">
+              <li class="links">FHA-HERO</li>
+            </a>  
+
+            <a target="_blank" href="http://www.naccp.org">
+              <li class="links">National Association of Child Care Professionals</li>
+            </a>
+
+            <a target="_blank" href="http://www.naeyc.org">
+              <li class="links">National Association for Education of Young Children</li>
+            </a>
+
+            <a target="_blank" href="http://www.eatright.org">
+              <li class="links">American Dietetic Association</li>
+            </a>
+
+             <a target="_blank" href="http://www.faseb.org">
+              <li class="links">American Assoc. for Nutritional Sciences</li>
+            </a>
+
+            <a target="_blank" href="http://www.acfchefs.org">
+              <li class="links">American Culinary Assoc</li>
+            </a>
+
+            <a target="_blank" href="http://www.sen.ca.gov">
+              <li class="links">California Legislative Links</li>
+            </a>
+
+            <a target="_blank" href="http://calrest.org">
+              <li class="links">California Restaurant Assoc</li>
+            </a>
+
+            <a target="_blank" href="http://chrie.org">
+              <li class="links">Council on Hotel, Restaurant, and Institutional Education</li>
+            </a>
+
+            <a target="_blank" href="http://www.gocalif.ca.gov">
+              <li class="links">California Division of Tourism</li>
+            </a>
+
+            <a target="_blank" href="http://www.asid.org">
+              <li class="links">American Assoc. of Interior Designers</li>
+            </a>
+
+            <a target="_blank" href="http://www.idec.org">
+              <li class="links">Interior Design Educators Council</li>
+            </a>
+
+            <a target="_blank" href="http://www.americanapparel.org">
+              <li class="links">American Apparel Manufacturers Assoc</li>
+            </a>
+
+            <a target="_blank" href="http://www.fgi.org">
+              <li class="links">Fashion Group International</li>
+            </a>
+
+            <a target="_blank" href="http://www.ncoa.org">
+              <li class="links">National Council on Aging</li>
+            </a>
+
+            <a target="_blank" href="http://www.nfcc.org">
+              <li class="links">Consumer Credit Counseling Service</li>
+            </a>
+
+            <a target="_blank" href="http://www.cteonline.org">
+              <li class="links">CTE Online</li>
+            </a>
+
+            <a target="_blank" href="https://www.memberplanet.com/FCSTAC">
+              <li class="links">Member Planet</li>
+            </a>
+          </ul>
+        </div>
     		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
