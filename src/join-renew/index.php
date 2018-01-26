4d4158fb23bb84fe06555f2772cb9b93b7cdab7c
NIBITPIE <jacob@detailsit.com>
2018-01-26 11:13:08 -0800

    Update index.php

--- src/join-renew/index.php
+++ src/join-renew/index.php
@@ -12,11 +12,12 @@
 	    <div class="container text-center" style="padding: 80px; 40px;">
 	    <h1 style="font-weight: 300">Membership</h1>
 	    <p style="padding: 20px 20px;"> Please click the link to join or renew membership.</p>
-      <a href="www.memberplanet.com/FCSTAC">
+      <a href="https://www.memberplanet.com/FCSTAC">
 	      Go to Membership Planet
 	    </a>
 	    </div>
 
+	    
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
