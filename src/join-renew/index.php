3685bd8a642047016cc51eddf7a702e28ea33d63
NIBITPIE <jacob@detailsit.com>
2018-01-26 07:44:17 -0800

    Update index.php

--- src/join-renew/index.php
+++ src/join-renew/index.php
@@ -11,7 +11,9 @@
       <?php include'../navbar.php';?>
 	    <h1 style="font-weight: 300">History</h1>
 	    <p style="padding: 20px 20px;"> Please click the link to join or renew membership.</p>
-      <a href="www.memberplanet.com/FCSTAC"></a>
+      <a href="www.memberplanet.com/FCSTAC">
+	      Go to Membership Planet
+	    </a>
 
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
