1d3375d73485b903a914c00400b61582b7be34a6
NIBITPIE <jacob@detailsit.com>
2018-01-26 07:39:41 -0800

    Update index.php

--- src/join-renew/index.php
+++ src/join-renew/index.php
@@ -14,7 +14,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
       <h1 style="font-weight: 300">History</h1>
-			<p style="padding: 20px 20px;"> Please click the link to join or renew membership.
+	    <p style="padding: 20px 20px;"> Please click the link to join or renew membership.</p>
       <a href="www.memberplanet.com/FCSTAC"></a>
     </body>
 </html>
