04c3b6cab292d3288f1745a1f89906bc2e3053d7
JustinRisney <31742964+JustinRisney@users.noreply.github.com>
2018-02-05 11:06:32 -0800

    Fixed some retardation by jacob

--- src/join-renew/index.php
+++ src/join-renew/index.php
@@ -4,17 +4,27 @@
         <title>FCTAC | Join/Renew</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" type="text/css" href="stylesheets/index.css">
+        <link rel="stylesheet" type="text/css" href="/stylesheets/index.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <style type="text/css">
+            #banner {
+                
+            }
+
+        </style>
     </head>
     <body>
       <?php include'../navbar.php';?>
 	    <div class="container text-center" style="padding: 80px; 40px;">
 	   	 <h1 style="font-weight: 300">Membership</h1>
 	  	  <p style="padding: 20px 20px;"> Please click the link to join or renew membership.</p>
-		    <a href="https://www.memberplanet.com/FCSTAC">Member Planet</a>
+		    <p>
+                <a href="https://www.memberplanet.com/FCSTAC">Member Planet</a>
+            </p>
+           
+            <img class="col-md-12" id="banner" src="/images/Banner.png"/>
 	    </div>
-	   <img src="/images/Banner.png" />
+	   
 	    
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
