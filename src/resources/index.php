eceace7783f31708737b4562c678cd3c40dfc121
natt-orious <37000168+natt-orious@users.noreply.github.com>
2018-03-02 11:54:56 -0800

    Update index.php

--- src/resources/index.php
+++ src/resources/index.php
@@ -90,6 +90,8 @@
             <a target="_blank" href="https://www.memberplanet.com/FCSTAC">
               <li class="links">Member Planet</li>
             </a>
+	    <a img class="col-md-12" img src="/images/Banner.png">
+      	    </a>
           </ul>
         </div>
     		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
