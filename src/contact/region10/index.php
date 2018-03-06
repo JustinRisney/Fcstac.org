c421d10a79178ef0430b4e267cbe58c178e16a1b
adumbidiot <nathaniel.daniel23@outlook.com>
2018-03-06 12:07:52 -0800

    Update styling on regions 1 and 10

--- src/contact/region10/index.php
+++ src/contact/region10/index.php
@@ -8,16 +8,27 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 		<style>
 			.title{
-				padding-top: 20px;
+				font-weight: 200;
 			}
 		</style>
 	</head>
     <body>
 		<?php include $_SERVER['DOCUMENT_ROOT'] . '/navbar.php';?>
 		<div class="container-fluid text-center" style="padding-top: 66px;">
-			<h1 class="title">Region 10</h1>
-			<h2 class="title">Cindy Gavin</h2>
-			<p>Email: <a href="mailto:cindigavin@gmail.com">cindigavin@gmail.com</a></p>
+			<div class="row">
+				<div class ="col-md-12" style="padding-top: 20px; padding-bottom: 40px;">
+					<h1 class="title">Region 10</h1>
+				</div>
+			</div>	
+			<div class="row justify-content-center">
+				<div class ="col-md-12" style="padding-top: 20px; padding-bottom: 10px;">
+					<h1 style="font-weight: 200;">Contacts</h1>
+				</div>
+				<div class="col-sm-6" style="border: 2px solid #2060C7; border-radius: 100px;">
+					<h2 class="title">Cindy Gavin</h2>
+					<p>Email: <a href="mailto:cindigavin@gmail.com">cindigavin@gmail.com</a></p>
+				</div>
+			</div>
 		</div>	
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
