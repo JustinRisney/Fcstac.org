730e63e13c4b863fc502fe23785bf5a8c7b04fee
adumbidiot <nathaniel.daniel23@outlook.com>
2018-03-08 11:33:08 -0800

    Styled all pages

--- src/contact/region6/index.php
+++ src/contact/region6/index.php
@@ -8,17 +8,30 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 		<style>
 			.title{
-				padding-top: 20px;
+				font-weight: 200;
 			}
 		</style>
 	</head>
     <body>
 		<?php include $_SERVER['DOCUMENT_ROOT'] . '/navbar.php';?>
 		<div class="container-fluid text-center" style="padding-top: 66px;">
-			<h1 class="title">Region 6</h1>
-			<h2 class="title">Tammie Meyer</h2>
-			<p>Email: <a href="mailto:tmeyer@muhsd.org">tmeyer@muhsd.org</a></p>
-		</div>	
+			<div class="row">
+				<div class ="col-md-12" style="padding-top: 20px; padding-bottom: 40px;">
+					<h1 class="title">Region 6</h1>
+				</div>
+			</div>	
+			<div class="row justify-content-center">
+				<div class ="col-md-12" style="padding-top: 20px; padding-bottom: 10px;">
+					<h1 style="font-weight: 200;">Contacts</h1>
+				</div>
+				<div class="col-sm-6" style="border: 2px solid #2060C7; border-radius: 100px;">
+					<h2 class="title">Tammie Meyer</h2>
+					<p style="font-weight:200;">
+						Email: <a href="mailto:tmeyer@muhsd.org">tmeyer@muhsd.org</a>
+					</p>
+				</div>
+			</div>
+		</div>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
