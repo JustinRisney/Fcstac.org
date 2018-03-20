9260969eea17caa40e2188e1c51b5153ea603284
JustinRisney <31742964+JustinRisney@users.noreply.github.com>
2018-03-20 12:03:58 -0700

    fixed some retardation part 7

--- src/advocacy/index.php
+++ src/advocacy/index.php
@@ -6,9 +6,64 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" type="text/css" href="stylesheets/index.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <style type="text/css">
+            .thiccc {
+                font-weight: 700;
+            } 
+
+            .blue-shit {
+                color: #2060C7;
+            }
+
+            .thicccer {
+                padding-top: 50px;
+            }
+
+        </style>
     </head>
     <body>
-      <?php include'../navbar.php';?>
+      <?php include '../navbar.php';?>
+            <div class="container-fluid text-center">
+                <div class="thicccer" id="AB-166">
+                    <h3 class="blue-shit">
+                        AB 166:  EDUCATIONAL PLAN FOR THE TEACHING OF FINANCIAL LITERACY
+                    </h3>
+                    <p class="thiccc">By, Lisa Burson</p>
+                    <p class="un-thiccc">This bill requires the state board to integrate financial literacy, including, but not limited to, budgeting and managing credit, student loans, consumer debt, and identity theft security with the next revision of textbooks or curriculum frameworks in the social sciences, health and mathematics curricula.  Section 51284 of the Education code was amended and filed with the Secretary of State August 26, 2013.
+                    </p>
+                </div>
+
+                <div class="thicccer" id="AB-1330">
+                    <h3 class="blue-shit">
+                        LEGISLATIVE UPDATE: AB 1330
+                    </h3>
+                   
+                    <p class="un-thiccc"> AB 1330 written by Teri Burns, Chief Legislative Advocate.
+
+                        The passage of AB 1330 (Furutani) adds the completion of a course in career technical education (CTE), as an alternative to the California high school graduation requirement that a pupil complete a course in visual or performing arts or foreign language. This law creates an option for local education agencies (LEAs) beginning in the 2012-13 school year.
+
+                        For LEAs looking for guidance implementing changes in response to this law, it is important to note that the bill does not require a governing board to adopt this option, nor does it require a board that does not currently offer CTE courses to create new programs. Additionally, once an LEA begins offering the option, it may elect to stop offering the option at any time.
+                    </p>
+                </div>
+
+                <div class="thicccer" id="SB-147">
+                    <h3 class="blue-shit">
+                        LEGISLATIVE UPDATE: SB 147
+                    </h3>
+
+                    <p class="un-thiccc">SB 147, authored by Senator Mark DeSaulnier, was signed into law by Governor Arnold Schwarzenegger on October 11, 2009. This bill requires the California State University to establish model academic standards for high school courses, including career technical education courses, for pupils who wish to attend the California State University.
+
+                        Beginning January 1, 2014, the act requires the California State University to recognize completion of all high school career technical education courses that meet model curriculum standards established by the Superintendent of Public Instruction as satisfying the completion of a general elective course requirement for the purpose of admission to that university.
+                    </p>
+
+                </div>
+
+
+
+
+            </div>
+
+
 
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
