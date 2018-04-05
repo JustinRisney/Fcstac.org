06f75c081039c0607304b04d5ea4e9c998cb00c0
adumbidiot <nathaniel.daniel23@outlook.com>
2018-04-05 11:24:09 -0700

    About Page update
    Updated and restyled page

--- src/about/index.php
+++ src/about/index.php
@@ -4,11 +4,9 @@
         <title>FCSTAC | About</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" type="text/css" href="stylesheets/index.css">
+        <link rel="stylesheet" type="text/css" href="/stylesheets/index.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-
         <style type="text/css">
-             
 			.mission-statement {
 				padding-top: 8px;
 				list-style-type: none;
@@ -18,31 +16,39 @@
     <body>
 		<?php include'../navbar.php';?>
 		<div class="container-fluid text-center" style="padding: 80px; 40px;">
-			<h1 style="font-weight: 300">About</h1>
-			<p style="padding: 20px 20px;">FCSTAC is an influential and effective organization that advocates for Family and Consumer Science teachers on issues impacting their programs and curriculum.</p>
-			
-			<h1 style="font-weight: 300">History</h1>
-			<p style="padding: 20px 20px;">HETAC (Home Economics Teachers Association of California) was formed in July 1981 after a survey of California home economics teachers indicated a need for an organization that would focus on issues concerning secondary and adult CFS and HERO programs in California.  In 2017, the name was changed to Family and Consumer Sciences Teachers Association of California (FCSTAC) to reflect the changing language used at the state department of education and the names more commonly used across the state for Family and Consumer Science classes.  FCSTAC elects officers to serve a 2-year term, has quarterly executive board meetings, assists with many coordinated professional development opportunities for California Family and Consumer Sciences Teachers. FCSTAC is proud to sponsor Teacher of the Year, Dream Dare and Do, FCCLA FCSTAC Scholarship and FCCLA Honor Chapters. Currently, FCSTAC is engaged in legislative action to ensure permanent, appropriate funding and CDE support for Family and Consumer Sciences programs across the state.
-			</p>
-			
-			<h1 style="font-weight: 300">Mission and Purpose</h1>
-			<p style="padding: 20px 20px;">
-				<ul >
-					<li class="mission-statement">To increase networking and communication among Family and Consumer Sciences teachers</li>
-					<li class="mission-statement">To keep teachers informed of the latest educational issues </li>
-					<li class="mission-statement">To encourage high standards of teaching </li>
-					<li class="mission-statement">To expand and improve public knowledge of Family and Consumer Sciences </li>
-					<li class="mission-statement">FCSTAC is a professional organization focusing on the unique needs of Family and Consumer Sciences teachers</li>
-					<li class="mission-statement">To actively promote the interests of Family and Consumer Science teachers </li>
-					<li class="mission-statement">To support FCCLA as an integral part of Family and Consumer Sciences programs</li>
-					<li class="mission-statement">To recognize and honor individuals for outstanding contributions to Family and Consumer Sciences Education</li>
-				</ul>
-
-			</p>
-			<img src="https://www.kids-cooking-activities.com/image-files/teenagechefcookingclass.jpg" class="col-md-12" />
+			<div class="row justify-content-center">
+				<div class="col-md-12">
+					<h1 style="font-weight: 300">About</h1>
+					<p>
+						FCSTAC is an influential and effective organization that advocates for Family and Consumer Science teachers on issues impacting their programs and curriculum.
+					</p>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-sm-6">
+					<h1 style="font-weight: 300">History</h1>
+					<p>
+						HETAC (Home Economics Teachers Association of California) was formed in July 1981 after a survey of California home economics teachers indicated a need for an organization that would focus on issues concerning secondary and adult CFS and HERO programs in California.  In 2017, the name was changed to Family and Consumer Sciences Teachers Association of California (FCSTAC) to reflect the changing language used at the state department of education and the names more commonly used across the state for Family and Consumer Science classes.  FCSTAC elects officers to serve a 2-year term, has quarterly executive board meetings, assists with many coordinated professional development opportunities for California Family and Consumer Sciences Teachers. FCSTAC is proud to sponsor Teacher of the Year, Dream Dare and Do, FCCLA FCSTAC Scholarship and FCCLA Honor Chapters. Currently, FCSTAC is engaged in legislative action to ensure permanent, appropriate funding and CDE support for Family and Consumer Sciences programs across the state.
+					</p>
+				</div>
+				<div class="col-sm-6">
+					<h1 style="font-weight: 300">Mission and Purpose</h1>
+					<ul style="text-align: left">
+						<li class="mission-statement">☙To increase networking and communication among Family and Consumer Sciences teachers</li>
+						<li class="mission-statement">☙To keep teachers informed of the latest educational issues </li>
+						<li class="mission-statement">☙To encourage high standards of teaching </li>
+						<li class="mission-statement">☙To expand and improve public knowledge of Family and Consumer Sciences </li>
+						<li class="mission-statement">☙FCSTAC is a professional organization focusing on the unique needs of Family and Consumer Sciences teachers</li>
+						<li class="mission-statement">☙To actively promote the interests of Family and Consumer Science teachers </li>
+						<li class="mission-statement">☙To support FCCLA as an integral part of Family and Consumer Sciences programs</li>
+						<li class="mission-statement">☙To recognize and honor individuals for outstanding contributions to Family and Consumer Sciences Education</li>
+					</ul>
+				</div>
+			</div>
+			<div class="row justify-content-center text-center">
+				<img src="https://www.kids-cooking-activities.com/image-files/teenagechefcookingclass.jpg" class="image-fluid"/>
+			</div>
 		</div>
-		
-		
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
