376922085e1b4383e47061579b8adf955f460382
adumbidiot <nathaniel.daniel23@outlook.com>
2018-05-01 11:48:36 -0700

    Work on Archived News Letters

--- src/news/index.php
+++ src/news/index.php
@@ -1,140 +1,133 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-<<<<<<< HEAD
         <title>FCSTAC | News</title>
-=======
-        <title>FCSTAC</title>
->>>>>>> baa00f6e2d7654602a86fefefe0c36fbe013dc77
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" type="text/css" href="/stylesheets/news.css">
+		<link rel="stylesheet" type="text/css" href="/stylesheets/index.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-    </head>
+		<style>
+			.news-header{
+				font-weight: 200;
+				font-size: 50px;
+			}
+			.news-link{
+				font-weight: 200;
+				font-size: 30px;
+			}
+			body{
+				width:100%;
+			}
+		</style>
+	</head>
     <body>
       <?php include'../navbar.php';?>
-           <div class="container-fluid">
-           
-
-
-
-
-
-               
-           </div>
-            <div id="link-container">
-
-             <br> <!--  <- Don't remove you bitch, nathanial-->
-            <h1>Archived News Letters</h1>
-
-            <ul style="list-style: none;">    
-                <li class="Year-published">
-                    <h3>2017</h3>
-                </li>
-                <li class="Links">
-                  <a href="/pdf/newsletters/Winter-Newsletter-2017.pdf">
-                        December Newsletter
-                  </a>
-                </li>
-                <li class="Links">
-                  <a href="/pdf/newsletters/Fall-Newsletter-2017.pdf">
-                        September Newsletter
-                  </a>
-                </li>
-                <li class="Year-published">
-                    <h3>2016</h3>
-                </li>
-                <li class="Links">
-                    <a href="/pdf/newsletters/December-Newsletter-2016.pdf">
-                        December Newsletter
-                    </a>
-                </li>
-                <li class="Links">
-                    <a href="/pdf/newsletters/September-Newsletter-2016.pdf">
-                        September Newsletter
-                    </a>
-                </li>
-                <li class="Links">
-                    <a href="/pdf/newsletters/May-Newsletter-2016.pdf">
-                        May Newsletter
-                    </a>
-                </li>
-
-                <li class="Year-published">
-                    <h3> 2015 </h3>
-                </li>
-
-                <li class="Links">
-                    <a href="/pdf/newsletters/Summer-Newsletter-2015.pdf">
-                        Summer Newsletter
-                    </a>
-                        
-                </li>
-                 <li class="Links">
-                    <a href="/pdf/newsletters/Fall-Newsletter-2015.pdf">
-                        Fall Newsletter
-                    </a>
-                </li>
-                 <li class="Links">
-                    <a href="/pdf/newsletters/Winter-Newsletter-2015.pdf">
-                        Winter Newsletter
-                    </a>
-                </li>
-                 <li class="Links">
-                    <a href="/pdf/newsletters/Spring-Newsletter-2015.pdf">
-                        Spring Newsletter
-                    </a>
-                </li>
-
-                 <li class="Year-published">
-                    
-                      <h3> 2014 </h3>
-                </li>
-                 <li class="Links">
-                    <a href="/pdf/newsletters/Winter-Newsletter-2014.pdf">
-                        Winter Newsletter
-                    </a>
-                </li>
-                 <li class="Links">
-                    <a href="/pdf/newsletters/Spring-Newsletter-2014.pdf">
-                        Spring Newsletter
-                    </a>
-                </li>
-                 <li class="Links">
-                    <a href="/pdf/newsletters/Summer-Newsletter-2014.pdf">
-                        Summer Newsletter
-                    </a>
-                </li>
-
-                 <li class="Year-published">
-                        <h3> 2013 </h3>
-                </li>
-                 <li class="Links">
-                    <a href="/pdf/newsletters/Spring-Newsletter-2013.pdf">
-                        Spring Newsletter
-                    </a>
-                </li>
-                 <li class="Links">
-                    <a href="/pdf/newsletters/Fall-Newsletter-2013.pdf">
-                        Fall Newsletter
-                    </a>
-                </li>
-
-                 <li class="Year-published">
-                     
-                        <h3>2012</h3>
-                </li>
-                 <li class="Links">
-                    <a href="/pdf/newsletters/Fall-Newsletter-2012.pdf">
-                        Fall Newsletter
-                    </a>
-                </li>
-              </ul>
-             <img src="http://adelledavis.org/wp-content/uploads/2010/06/DSC00817_t479.jpg" />
-             <img class="col-md-12" src="/images/Banner.png" />
-            </div>
-
-
+           <div class="container-fluid text-center">
+				<div class="row">
+					<div class="col-md-12">
+						 <h1 style="font-weight: 200; font-size:60px;">Archived News Letters</h1>
+					</div>
+					<div class="col-md-12">
+						<h3 class="news-header">2017</h3>
+						<div>
+							<a href="/pdf/newsletters/Winter-Newsletter-2017.pdf" class="news-link">
+								December Newsletter
+							</a>
+						</div>
+						<div>
+							<a href="/pdf/newsletters/Fall-Newsletter-2017.pdf" class="news-link">
+								September Newsletter
+							</a>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<h3 class="news-header">2016</h3>
+						<div>
+							<a href="/pdf/newsletters/December-Newsletter-2016.pdf" class="news-link">
+								December Newsletter
+							</a>
+						</div>
+						<div>
+							<a href="/pdf/newsletters/September-Newsletter-2016.pdf" class="news-link">
+								September Newsletter
+							</a>
+						</div>
+						<div>
+							<a href="/pdf/newsletters/May-Newsletter-2016.pdf" class="news-link">
+								 May Newsletter
+							</a>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<h3 class="news-header">2015</h3>
+						<div>
+							<a href="/pdf/newsletters/Summer-Newsletter-2015.pdf" class="news-link">
+								Summer Newsletter
+							</a>
+						</div>
+						<div>
+							<a href="/pdf/newsletters/Fall-Newsletter-2015.pdf" class="news-link">
+								Fall Newsletter
+							</a>
+						</div>
+						<div>
+							<a href="/pdf/newsletters/Winter-Newsletter-2015.pdf" class="news-link">
+								 Winter Newsletter
+							</a>
+						</div>
+						<div>
+							<a href="/pdf/newsletters/Spring-Newsletter-2015.pdf" class="news-link">
+								Spring Newsletter
+							</a>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<h3 class="news-header">2014</h3>
+						<div>
+							<a href="/pdf/newsletters/Winter-Newsletter-2014.pdf" class="news-link">
+								Winter Newsletter
+							</a>
+						</div>
+						<div>
+							<a href="/pdf/newsletters/Spring-Newsletter-2014.pdf" class="news-link">
+								Spring Newsletter
+							</a>
+						</div>
+						<div>
+							<a href="/pdf/newsletters/Summer-Newsletter-2014.pdf" class="news-link">
+								Summer Newsletter
+							</a>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<h3 class="news-header">2013</h3>
+						<div>
+							<a href="/pdf/newsletters/Spring-Newsletter-2013.pdf" class="news-link">
+								Spring Newsletter
+							</a>
+						</div>
+						<div>
+							<a href="/pdf/newsletters/Fall-Newsletter-2013.pdf" class="news-link">
+								Fall Newsletter
+							</a>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<h3 class="news-header">2012</h3>
+						<div>
+							<a href="/pdf/newsletters/Fall-Newsletter-2012.pdf" class="news-link">
+								Fall Newsletter
+							</a>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<br>
+						<img src="http://adelledavis.org/wp-content/uploads/2010/06/DSC00817_t479.jpg" />
+						<img class="col-md-12" src="/images/Banner.png" />
+					</div>
+				</div>
+			</div>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
