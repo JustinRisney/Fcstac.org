6ecf2a0cc7b13a296a9292260878594f827c241e
adumbidiot <nathaniel.daniel23@outlook.com>
2018-05-01 11:10:47 -0700

    Professional Development Page update

--- src/professional-development/index.php
+++ src/professional-development/index.php
@@ -4,18 +4,27 @@
         <title>FCSTAC | Professional Development</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" type="text/css" href="stylesheets/index.css">
+        <link rel="stylesheet" type="text/css" href="/stylesheets/index.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-    </head>
+		<style>
+			.header{
+				font-weight: 200;
+			}
+		</style>
+	</head>
     <body>
       <?php include'../navbar.php';?>
             <div class="text-center container-fluid">
-                <ul style="list-style: none;">
-                    <a target="_blank" href="http://ca-fcs.org/professional-development/workshops">
-                        California Family and Consumer Sciences Professional Development
-                    </a>
-                </ul>
-
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<h1 class="header">Professional Development</h1>
+					</div>
+					<div class="col-md-12">
+						<a target="_blank" href="http://ca-fcs.org/professional-development/workshops" style="font-size: 20px; font-weight:200;">
+							California Family and Consumer Sciences Professional Development
+						</a>
+					</div>
+				</div>
             </div>
 
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
